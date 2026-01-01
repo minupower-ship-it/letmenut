504e5767a3472601f10ec4e5bffb80d757e93c11
minupower-ship-it <minupower@gmail.com>
2026-01-01 14:52:31 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 TOKEN = os.environ.get("BOT_TOKEN")
 API_URL = f"https://api.telegram.org/bot{TOKEN}"
 
-VIDEO_URL = ""
+VIDEO_URL = "https://files.catbox.moe/3f3sul.mp4"
 
 ADMIN_ID = 5619516265
 
@@ -168,6 +168,7 @@ def webhook():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
